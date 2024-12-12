8c117bf8fbb2a8bed4ef37e7c71b18df4af9e11a
nuuuri <sbfl125@gmail.com>
2024-12-13 05:13:42 +0900

    [feat] @next/mdx 설정

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,9 @@
+import Test from '@/posts/test.mdx';
+
 export default function Home() {
   return (
     <div>
-      메인 페이지
-      <div className="w-[2000px] h-[3000px]" />
-      end
+      <Test />
     </div>
   );
 }
