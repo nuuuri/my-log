20932fcedf4011d59089777f96c4ba209ee0368c
nuuuri <sbfl125@gmail.com>
2024-12-17 17:56:25 +0900

    [fix] ul key 수정

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -8,7 +8,7 @@ import Skills from '@/components/resume/Skills';
 
 export default function AboutPage() {
   return (
-    <div className="max-w-5xl px-20 mx-auto tracking-wide font-gothicA1">
+    <div className="max-w-6xl px-20 mx-auto tracking-wide font-gothicA1">
       <h1 className="mb-1 text-4xl font-bold tracking-widest">박윤수</h1>
       <h3 className="ml-[2px] font-medium text-2xl">Frontend Engineer</h3>
       <div className="flex items-center justify-between gap-10">
--- src/components/resume/common/List.tsx
+++ src/components/resume/common/List.tsx
@@ -12,13 +12,12 @@ export default function List({ className, data, level = 1 }: ListProps) {
       {data.map(
         ({ text, children }, idx) =>
           text && (
-            <>
+            <div key={idx}>
               <li
-                key={idx}
                 className={`py-[2px] tracking-normal last:pb-1 ${level === 1 ? 'mt-1 font-medium text-[17px] dark:!text-white' : 'font-light text-base'} ${className}`}
                 dangerouslySetInnerHTML={{ __html: text }}></li>
               {children && <List data={children} level={level + 1} />}
-            </>
+            </div>
           )
       )}
     </ul>
