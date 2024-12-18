f4fb0b4e0941c20def44b62d3a069be7f80ea2ae
nuuuri <sbfl125@gmail.com>
2024-12-19 01:52:59 +0900

    [chore] github, blog 주소 추가

--- src/components/resume/Contact.tsx
+++ src/components/resume/Contact.tsx
@@ -2,10 +2,21 @@ import Link from 'next/link';
 
 export default function Contact() {
   return (
-    <div className="flex flex-col justify-center flex-shrink-0 w-full p-6 border rounded-md border-zinc-200 md:w-fit">
+    <div className="flex flex-col justify-center flex-shrink-0 w-full gap-1 p-6 border rounded-md border-zinc-200 md:w-fit">
       <div>🎂 1997.03.21</div>
       <div>📨 sbfl125@gmail.com</div>
-      <Link href="https://github.com/nuuuri">💻 https://github.com/nuuuri</Link>
+      <Link
+        href="https://github.com/nuuuri"
+        rel="noopener noreferrer"
+        target="_blank">
+        💻 https://github.com/nuuuri
+      </Link>
+      <Link
+        href="https://nuuuri.vercel.app/"
+        rel="noopener noreferrer"
+        target="_blank">
+        📝 https://nuuuri.vercel.app/
+      </Link>
     </div>
   );
 }
--- src/components/resume/common/List.tsx
+++ src/components/resume/common/List.tsx
@@ -14,7 +14,7 @@ export default function List({ className, data, level = 1 }: ListProps) {
           text && (
             <div key={idx}>
               <li
-                className={`py-[2px] last:pb-1 ${level === 1 ? 'mt-1 !font-medium text-[17px] tracking-normal dark:!text-white' : 'font-light text-base tracking-tight'} ${className}`}
+                className={`py-[2px] last:pb-1 ${level === 1 ? 'mt-1 !font-medium text-[17px] tracking-normal dark:!text-white' : 'font-light text-[15px] tracking-tight'} ${className}`}
                 dangerouslySetInnerHTML={{ __html: text }}></li>
               {children && <List data={children} level={level + 1} />}
             </div>
